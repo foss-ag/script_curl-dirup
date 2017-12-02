4d9b6ef02f9a70bd88d327b8848df554ec085b60
Michael Gajda <michael.gajda@tu-dortmund.de>
2017-12-02 19:28:45 +0100

    Add DAV upload script

---
+++ curl-dirup.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+# 2017 Michael Gajda <draget@speciesm.net>
+# See LICENSE for license information
+
+USAGE="$0 <URL> <USER>:<PW> <DIR>"
+
+# check parameter
+: ${1?Please specify a WebDAV URL. Usage: $USAGE}
+: ${2?Please specify a user and password. Usage: $USAGE}
+: ${3?Please specify a directory. USAGE $USAGE}
+
+# check path
+case $3 in
+/*|.*)
+    echo "Please specify a relative subdirectory. Usage: $USAGE" ; exit 1 ;;
+esac
+
+# create dirs relative to current recursively
+find $3 -type d ! -path . -exec curl --user $2 -X MKCOL $1/$(realpath --relative-to="." {}) \;
+# upload files recursively
+find $3 -type f -exec curl --user $2 -T {} $1/{} \;
